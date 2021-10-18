cf61bf8a4574c4031f9aeb14053867c8e3d3f217
rikicop <ruperto1@protonmail.com>
2021-10-18 15:55:53 -0400

    end

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -22,6 +24,8 @@ app.use(express.json());
 //y digo que ese endpoint pertenece a authRoute
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("El servidor Backend está corriendo!");
